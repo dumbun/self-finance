063504168b5bd50236818e1570122601d6512d59
Vamshi Krishna <vamsikrishna2644@gmail.com>
2023-10-30 22:16:31 +0000

    added logic

---
+++ lib/logic/logic.dart
@@ -0,0 +1,50 @@
+import 'package:intl/intl.dart';
+
+class LoanCalculator {
+  final int takenAmount;
+  final dynamic rateOfInterest;
+  final String takenDate;
+  final DateTime todayDate;
+
+  LoanCalculator({required this.takenAmount, required this.rateOfInterest, required this.takenDate})
+      : todayDate = DateTime.now();
+
+  int getDays() {
+    final DateFormat format = DateFormat("dd-MM-yyyy");
+    final DateTime convertedDate = format.parseStrict(takenDate);
+    final differenceBetweenDays = todayDate.difference(convertedDate).inDays;
+    return differenceBetweenDays;
+  }
+
+  String daysToMonthsAndRemainingDays() {
+    int days = getDays();
+    return "${(days ~/ 30)} Months - ${days % 30} Days";
+  }
+
+  double getInterestPerDay() {
+    final num perMonth = takenAmount * (rateOfInterest / 100);
+    final double perDay = perMonth / 30;
+    return perDay;
+  }
+
+  double totalInterest() {
+    final double get = getInterestPerDay();
+    final int totalDays = getDays();
+    final double totalInterest = get * totalDays;
+    return totalInterest;
+  }
+
+  double getTotal() {
+    int getdays() {
+      final DateFormat format = DateFormat("dd-MM-yyyy");
+      final DateTime convertedDate = format.parseStrict(takenDate);
+      final differenceBetweenDays = todayDate.difference(convertedDate).inDays;
+      return differenceBetweenDays;
+    }
+
+    final double interestPerDay = getInterestPerDay();
+    final int totalDays = getdays();
+    final double totalInterest = interestPerDay * totalDays;
+    return takenAmount + totalInterest;
+  }
+}
