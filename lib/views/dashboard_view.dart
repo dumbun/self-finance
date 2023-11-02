4ab99008bffa7d453a5b67459fddc66cdcb19762
Vamshi Krishna <vamsikrishna2644@gmail.com>
2023-11-02 22:02:35 +0000

    added the bottom navigation bar and  3 new views and a action button

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:self_finance/constants/routes.dart';
 import 'package:self_finance/models/user_model.dart';
-import 'package:self_finance/widgets/round_corner_button.dart';
+import 'package:self_finance/theme/colors.dart';
+import 'package:self_finance/views/emi_calculator.dart';
+import 'package:self_finance/views/history.dart';
+import 'package:self_finance/views/home_screen.dart';
 
 class DashboardView extends StatefulWidget {
-  const DashboardView({super.key, required this.user});
+  const DashboardView({Key? key, required this.user}) : super(key: key);
 
   final User user;
 
@@ -13,24 +17,90 @@ class DashboardView extends StatefulWidget {
 }
 
 class _DashboardViewState extends State<DashboardView> {
+  late PageController _pageController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _selectedIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Dashoard"),
-            Text(widget.user.userName),
-            Text(widget.user.userPin),
-            Text(widget.user.profilePicture.toString()),
-            RoundedCornerButton(
-              text: "add new entry",
-              onPressed: () => Routes.navigateToAddNewEntry(context: context),
-            )
+      body: SafeArea(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          children: const <Widget>[
+            HomeScreen(),
+            EmiCalculator(),
+            HistoryView(),
           ],
         ),
       ),
+      floatingActionButton: _FloatingActionButton(selectedIndex: _selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate_outlined),
+            label: 'EMI calculator',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history_rounded),
+            label: 'History',
+          ),
+        ],
+        selectedItemColor: getPrimaryColor,
+        currentIndex: _selectedIndex,
+        enableFeedback: true,
+        onTap: (index) {
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+          );
+        },
+      ),
     );
+  }
+}
+
+class _FloatingActionButton extends StatelessWidget {
+  const _FloatingActionButton({Key? key, required this.selectedIndex}) : super(key: key);
+
+  final int selectedIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    if (selectedIndex == 0) {
+      return FloatingActionButton(
+        clipBehavior: Clip.antiAlias,
+        foregroundColor: getPrimaryColor,
+        elevation: 2.sp,
+        onPressed: () => Routes.navigateToAddNewEntry(context: context),
+        enableFeedback: true,
+        mini: false,
+        shape: const CircleBorder(),
+        backgroundColor: getPrimaryColor,
+        tooltip: "Add New Entry",
+        child: const Icon(
+          Icons.add_rounded,
+          color: getBackgroundColor,
+          size: 25,
+        ),
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
   }
 }
