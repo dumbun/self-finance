f97223c29f7d6baa747f784a22201fd813e02f9d
Vamshi Krishna <vamsikrishna2644@gmail.com>
2023-11-23 01:34:04 +0000

    clean up

--- lib/widgets/image_picker_widget.dart
+++ lib/widgets/image_picker_widget.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-import 'package:self_finance/constants/constants.dart';
 import 'package:self_finance/fonts/body_text.dart';
-import 'package:self_finance/theme/colors.dart';
 import 'package:self_finance/util.dart';
 import 'package:self_finance/widgets/image_widget.dart';
 
