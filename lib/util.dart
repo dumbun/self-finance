483947736cd4b7d6936677b51de350e7f1a7b1ae
Vamshi Krishna <vamsikrishna2644@gmail.com>
2023-12-11 03:36:21 +0000

    caching images based on device ram size

--- lib/util.dart
+++ lib/util.dart
@@ -1,14 +1,45 @@
+// ignore_for_file: avoid_print
+
 import 'dart:convert';
 import 'dart:core';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:typed_data';
 import 'package:intl/intl.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class ImageCacheManager {
   static final Map<String, Image> _imageCache = {};
-  static const int _maxCacheSize = 50; // Maximum number of images to cache
+  static int _maxCacheSize = 100;
+
+  static const platform = MethodChannel('ramInfoChannel');
+
+  static Future<void> updateMaxCacheSize() async {
+    try {
+      final totalRAM = await platform.invokeMethod('getTotalRAM');
+      _maxCacheSize = _calculateMaxCacheSize(totalRAM);
+    } on PlatformException catch (e) {
+      print("Failed to get RAM info: ${e.message}");
+      // Set a default cache size in case of failure
+      _maxCacheSize = 100;
+    }
+  }
+
+  static pr() async {
+    final totalRAM = await platform.invokeMethod('getTotalRAM');
+    print(totalRAM);
+  }
+
+  static int _calculateMaxCacheSize(int totalRAM) {
+    // Adjust cache size based on available RAM
+    if (totalRAM <= 1073741824) {
+      return 40; // 1 GB or less
+    } else if (totalRAM <= 2147483648) {
+      return 60; // 2 GB
+    } else {
+      return 80; // More than 2 GB
+    }
+  }
 
   static Image getCachedImage(String base64String, double? height, double? width) {
     if (_imageCache.containsKey(base64String)) {
@@ -20,7 +51,7 @@ class ImageCacheManager {
         base64Decode(base64String),
         fit: BoxFit.fill,
         height: height ?? 40.sp,
-        width: width ?? 40.sp,
+        width: height ?? 40.sp,
       );
 
       if (_imageCache.length >= _maxCacheSize) {
@@ -33,13 +64,11 @@ class ImageCacheManager {
   }
 
   static void _reorderCache(String key, Image value) {
-    // Move accessed image to the end to simulate LRU behavior
     _imageCache.remove(key);
     _imageCache[key] = value;
   }
 
   static void _removeLeastUsedImage() {
-    // Remove the least recently used image
     _imageCache.remove(_imageCache.keys.first);
   }
 }
