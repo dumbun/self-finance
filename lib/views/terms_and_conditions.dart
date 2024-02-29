11af63bba44e2394003f2b139a46276feff1e42c
Vamshi Krishna <vamsikrishna2644@gmail.com>
2024-02-29 14:49:57 +0000

    version changed and andriod build settings changed and debug methods and code migration also changed to flutter new version 3.16

--- lib/views/terms_and_conditions.dart
+++ lib/views/terms_and_conditions.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:self_finance/constants/constants.dart';
 import 'package:self_finance/constants/routes.dart';
