7edda100254167757405eed6c1f9df823eceef54
Vamshi Krishna <vamsikrishna2644@gmail.com>
2024-05-13 05:36:46 +0100

    added the security fetures

--- android/app/src/main/kotlin/com/vamshikrishna/self_finance/MainActivity.kt
+++ android/app/src/main/kotlin/com/vamshikrishna/self_finance/MainActivity.kt
@@ -2,6 +2,13 @@ package com.vamshikrishna.self_finance
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
+import android.view.WindowManager.LayoutParams
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
 class MainActivity: FlutterFragmentActivity() {
-    
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    window.addFlags(LayoutParams.FLAG_SECURE)
+    super.configureFlutterEngine(flutterEngine)
+  }
 }
