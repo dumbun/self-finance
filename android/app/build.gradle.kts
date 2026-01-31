9bf5285d5344d9f968cbeea3b68b96342e0fd7dc
Vamshi Krishna <vamsikrishna2644@gmail.com>
2026-01-31 15:58:28 +0530

    app bundle changed to com.dumbun.self_finance

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.vamshikrishna.self_finance"
+    namespace = "com.dumbun.self_finance"
     compileSdk = flutter.compileSdkVersion
     //ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.vamshikrishna.self_finance"
+        applicationId = "com.dumbun.self_finance"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/dumbun/self_finance/MainActivity.kt
+++ android/app/src/main/kotlin/com/dumbun/self_finance/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.vamshikrishna.self_finance
+package com.dumbun.self_finance
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
